3b751790fe07cf7a914e486f102c2cccc5ddd5be
fascinated <liam@fascinated.cc>
2026-01-31 09:58:16 +0000

    remove download

--- src/main/java/xyz/mcutils/backend/controller/PlayerController.java
+++ src/main/java/xyz/mcutils/backend/controller/PlayerController.java
@@ -54,17 +54,14 @@ public class PlayerController {
     @GetMapping(value = "/{id}/skin.{extension}")
     public ResponseEntity<?> getPlayerSkin(
             @Parameter(description = "The UUID or Username of the player", example = "ImFascinated") @PathVariable String id,
-            @Parameter(description = "The file extension of the image", example = "png") @PathVariable String extension,
-            @Parameter(description = "Whether to download the image") @RequestParam(required = false, defaultValue = "false") boolean download) {
+            @Parameter(description = "The file extension of the image", example = "png") @PathVariable String extension) {
         CachedPlayer cachedPlayer = playerService.getPlayer(id);
-        String dispositionHeader = download ? "attachment; filename=%s.png" : "inline; filename=%s.png";
 
         // Return the part image
         Skin skin = cachedPlayer.getPlayer().getSkin();
         return ResponseEntity.ok()
                 .cacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic())
                 .contentType(extension.equals("png") ? MediaType.IMAGE_PNG : MediaType.IMAGE_JPEG)
-                .header(HttpHeaders.CONTENT_DISPOSITION, dispositionHeader.formatted(skin.getId()))
                 .body(skinService.getSkinImage(skin));
     }
 
@@ -74,17 +71,13 @@ public class PlayerController {
             @Parameter(description = "The part of the skin", example = "head") @PathVariable String part,
             @Parameter(description = "The file extension of the image", example = "png") @PathVariable String extension,
             @Parameter(description = "The size of the image", example = "256") @RequestParam(required = false, defaultValue = "256") int size,
-            @Parameter(description = "Whether to render the skin overlay (skin layers)", example = "false") @RequestParam(required = false, defaultValue = "false") boolean overlays,
-            @Parameter(description = "Whether to download the image") @RequestParam(required = false, defaultValue = "false") boolean download) {
-        CachedPlayer cachedPlayer
-                = playerService.getPlayer(id);
-        String dispositionHeader = download ? "attachment; filename=%s.png" : "inline; filename=%s.png";
+            @Parameter(description = "Whether to render the skin overlay (skin layers)", example = "false") @RequestParam(required = false, defaultValue = "false") boolean overlays) {
+        CachedPlayer cachedPlayer = playerService.getPlayer(id);
 
         // Return the part image
         return ResponseEntity.ok()
                 .cacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic())
                 .contentType(extension.equals("png") ? MediaType.IMAGE_PNG : MediaType.IMAGE_JPEG)
-                .header(HttpHeaders.CONTENT_DISPOSITION, dispositionHeader.formatted(cachedPlayer.getPlayer().getUsername()))
                 .body(skinService.getSkinPart(cachedPlayer.getPlayer(), part, overlays, size).getBytes());
     }
 }
