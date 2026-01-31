6eebfc41bdcbffd8086bf8b0f2227c7d939e28e7
fascinated <liam@fascinated.cc>
2026-01-31 10:47:40 +0000

    imports

--- src/main/java/xyz/mcutils/backend/controller/PlayerController.java
+++ src/main/java/xyz/mcutils/backend/controller/PlayerController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/xyz/mcutils/backend/controller/ServerController.java
+++ src/main/java/xyz/mcutils/backend/controller/ServerController.java
@@ -3,7 +3,6 @@ package xyz.mcutils.backend.controller;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
