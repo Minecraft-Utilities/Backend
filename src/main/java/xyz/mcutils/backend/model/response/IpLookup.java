dfd521f62f68c2c22849ba2e42f209e115be08d4
fascinated <liam@fascinated.cc>
2026-02-03 09:44:57 +0000

    add missing docs

--- src/main/java/xyz/mcutils/backend/model/response/IpLookup.java
+++ src/main/java/xyz/mcutils/backend/model/response/IpLookup.java
@@ -3,4 +3,12 @@ package xyz.mcutils.backend.model.response;
 import xyz.mcutils.backend.model.asn.AsnLookup;
 import xyz.mcutils.backend.model.geo.GeoLocation;
 
+/**
+ * A record that contains the IP lookup information.
+ *
+ * @param ip the IP address
+ * @param reverseDns the reverse DNS of the IP address
+ * @param location the location of the IP address
+ * @param asn the ASN of the IP address
+ */
 public record IpLookup(String ip, String reverseDns, GeoLocation location, AsnLookup asn) { }
