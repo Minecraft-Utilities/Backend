b57b8a49c158fca3da0657b03cbeb1faa27f3cb5
fascinated <liam@fascinated.cc>
2026-02-08 17:07:40 +0000

    fix bug

--- src/main/java/xyz/mcutils/backend/model/persistence/mongo/PlayerDocument.java
+++ src/main/java/xyz/mcutils/backend/model/persistence/mongo/PlayerDocument.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import xyz.mcutils.backend.model.domain.player.Player;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -67,6 +68,15 @@ public class PlayerDocument {
      * The date this player was first seen on.
      */
     private Date firstSeen;
+
+    public List<HistoryItem> getSkinHistory() {
+        return skinHistory  == null ? new ArrayList<>() : skinHistory;
+    }
+
+    public List<HistoryItem> getCapeHistory() {
+        return capeHistory == null ? new ArrayList<>() : capeHistory;
+    }
+
 
     /**
      * A history item used for Skins and Capes.
