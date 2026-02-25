6734c737d9be08e8083519b66c39fc5d7a21829d
fascinated <liam@fascinated.cc>
2026-02-25 21:05:53 +0000

    more

--- src/main/java/xyz/mcutils/backend/service/PlayerRefreshService.java
+++ src/main/java/xyz/mcutils/backend/service/PlayerRefreshService.java
@@ -43,10 +43,9 @@ import java.util.concurrent.Semaphore;
 @Slf4j
 public class PlayerRefreshService {
     private static final Duration MIN_TIME_BETWEEN_UPDATES = Duration.ofHours(1);
-    private static final int REFRESH_WORKER_THREADS = 100;
     private static final int HISTORY_OR_CHUNK = 1000;
 
-    private final Semaphore refreshConcurrencyLimit = new Semaphore(REFRESH_WORKER_THREADS);
+    private final Semaphore refreshConcurrencyLimit = new Semaphore(200);
     
     private final MojangService mojangService;
     private final SkinService skinService;
