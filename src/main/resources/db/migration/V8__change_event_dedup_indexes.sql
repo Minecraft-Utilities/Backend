9696498b6a4ca7418f2bd4c8c749a9d6ee031c81
fascinated <liam@fascinated.cc>
2026-05-15 13:01:52 +0100

    fix migration

--- src/main/resources/db/migration/V8__change_event_dedup_indexes.sql
+++ src/main/resources/db/migration/V8__change_event_dedup_indexes.sql
@@ -1,8 +1,8 @@
 CREATE UNIQUE INDEX idx_username_change_events_dedup
-    ON username_change_events (player_id, new_username, COALESCE(previous_username, ''), date_trunc('day', timestamp));
+    ON username_change_events (player_id, new_username, COALESCE(previous_username, ''), date_trunc('day', timestamp AT TIME ZONE 'UTC'));
 
 CREATE UNIQUE INDEX idx_skin_change_events_dedup
-    ON skin_change_events (player_id, skin_id, date_trunc('day', timestamp));
+    ON skin_change_events (player_id, skin_id, date_trunc('day', timestamp AT TIME ZONE 'UTC'));
 
 CREATE UNIQUE INDEX idx_cape_change_events_dedup
-    ON cape_change_events (player_id, cape_id, date_trunc('day', timestamp));
+    ON cape_change_events (player_id, cape_id, date_trunc('day', timestamp AT TIME ZONE 'UTC'));
