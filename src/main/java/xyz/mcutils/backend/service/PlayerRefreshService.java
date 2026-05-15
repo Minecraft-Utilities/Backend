ef6974e98dc8b615513b141b7a3f7721062349a0
fascinated <liam@fascinated.cc>
2026-05-15 20:42:30 +0100

    slighltly more

--- src/main/java/xyz/mcutils/backend/service/PlayerRefreshService.java
+++ src/main/java/xyz/mcutils/backend/service/PlayerRefreshService.java
@@ -28,7 +28,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Slf4j
 public class PlayerRefreshService {
     private static final int REFRESH_CHUNK_SIZE = 200;
-    private static final int RATE_LIMIT = 40;
+    private static final int RATE_LIMIT = 50;
 
     private final RateLimiter rateLimiter = RateLimiter.create(RATE_LIMIT);
     private final MojangService mojangService;
