66252d60244e22edfdd34a4e6d89b7c0a3a1e557
fascinated <liam@fascinated.cc>
2026-05-17 12:11:21 +0100

    add missing indexs

---
+++ src/main/resources/db/migration/V20__missing_index.sql
@@ -0,0 +1,5 @@
+CREATE INDEX CONCURRENTLY idx_cape_change_events_cape_id_timestamp
+ON cape_change_events (cape_id, timestamp ASC);
+
+CREATE INDEX CONCURRENTLY idx_skin_change_events_skin_id_timestamp
+ON skin_change_events (skin_id, timestamp ASC);
