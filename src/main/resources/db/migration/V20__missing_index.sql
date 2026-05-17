4c7b2f289d07943d6e6e2a8e8c25ff59cbae696d
Lee <liam@fascinated.cc>
2026-05-17 12:40:35 +0100

    Update V20__missing_index.sql

--- src/main/resources/db/migration/V20__missing_index.sql
+++ src/main/resources/db/migration/V20__missing_index.sql
@@ -1,5 +1,5 @@
-CREATE INDEX CONCURRENTLY idx_cape_change_events_cape_id_timestamp
+CREATE INDEX IF NOT EXISTS CONCURRENTLY idx_cape_change_events_cape_id_timestamp
 ON cape_change_events (cape_id, timestamp ASC);
 
-CREATE INDEX CONCURRENTLY idx_skin_change_events_skin_id_timestamp
+CREATE INDEX IF NOT EXISTS CONCURRENTLY idx_skin_change_events_skin_id_timestamp
 ON skin_change_events (skin_id, timestamp ASC);
