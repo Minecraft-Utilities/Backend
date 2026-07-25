8ba6e22c263c2a80c140b4b3333ea0922c1b2f03
fascinated <liam@fascinated.cc>
2026-07-25 19:27:08 +0100

    Add missing QueryHints import for refresh claim query.

--- src/main/java/xyz/mcutils/backend/repository/postgres/PlayerRepository.java
+++ src/main/java/xyz/mcutils/backend/repository/postgres/PlayerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import xyz.mcutils.backend.model.persistence.postgres.PlayerRow;
